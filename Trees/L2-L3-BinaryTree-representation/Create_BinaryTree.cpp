5f74d4a43d7f621df8011fb670ed25369cd2a69e
Ksama Arora <ksama2004.arora@gmail.com>
2024-08-12 19:34:25 +0530

    till inorder traversal

---
+++ Trees/L2-L3-BinaryTree-representation/Create_BinaryTree.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+struct node{
+    int data;
+    node *left;
+    node *right;
+};
+
+typedef struct node snode;
+
+snode* create_node(int val){
+    snode *newnode=new snode;
+    newnode->data=val;
+    newnode->left=NULL;
+    newnode->right=NULL;
+    return newnode;
+}
+
+int main(){
+    snode* root=create_node(1);
+    root->left=create_node(2);
+    root->right=create_node(3);
+    root->left->left=create_node(4);
+    root->left->right=create_node(5);
+    return 0;
+}
