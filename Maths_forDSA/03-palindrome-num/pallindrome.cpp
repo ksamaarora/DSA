9bff42a6fb652c1808ff83bcd84e39a24a5846b6
Ksama Arora <ksama2004.arora@gmail.com>
2024-10-11 01:02:41 +0530

    pallindrome m3 update

--- Maths_forDSA/03-palindrome-num/pallindrome.cpp
+++ Maths_forDSA/03-palindrome-num/pallindrome.cpp
@@ -31,3 +31,19 @@ public:
         return true;
     }
 };
+
+// Method 3: 
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+        int reversedHalf = 0;
+        while (x > reversedHalf) {
+            reversedHalf = reversedHalf * 10 + x % 10;
+            x /= 10; 
+        }
+        return x == reversedHalf || x == reversedHalf / 10;
+    }
+};
